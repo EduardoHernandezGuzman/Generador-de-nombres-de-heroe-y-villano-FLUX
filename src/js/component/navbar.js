1ca51e6a73c6a679a7581eed3daea9883c724cbd
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-12-14 18:33:50 +0000

    Trabajo en el navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ImagenLogoNavbar from "../../img/HV001.jpeg";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+
+
+
+
+
+
+
+		<nav className="navbar navbar-light  mb-3 estiloNavbar">
+
+
+			<div className="col-3">
+				<Link to="/">
+					<img className="imagenLogo" src={ImagenLogoNavbar} alt="LogoNavbar" />
 				</Link>
 			</div>
+
+			<div className="col">
+				<h1 className="titulo">GENERADOR DE NOMBRES DE <br></br> <span className="hyv">HÉROES</span> y <span className="hyv">VILLANOS</span></h1>
+			</div>
+			<div className="col3">
+
+			</div>
+
+
+			{/* <div className="ml-auto">
+        <Link to="/demo">
+          <button className="btn btn-primary">Check the Context in action</button>
+        </Link>
+      </div> */}
+
+
+	
 		</nav>
+
+
+
+
 	);
 };
+
